0faa4757ed44dfb6765623845a1c21f8a240d5ab
cyha <68947506+cyha@users.noreply.github.com>
2021-01-21 17:01:01 +0100

    views et variables

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about', ['name' => 'Cyril']);
+});
 
 Route::get('/contact', [ContactController::class, 'index']);
 
